7db7af614926cd8057b41c302762b487421c0a4e
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2023-03-01 11:53:20 +0700

    Update app.py

--- Final PDT/app.py
+++ Final PDT/app.py
@@ -462,3 +462,9 @@ def delete_user(user_id):
         return redirect('/')
     return render_template('users/delete.html',users=users)
 
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
